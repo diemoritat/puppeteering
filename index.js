405243cdf1cbcb8225501d15a7c60c82949cc47e
Nayara Alves <nayara.nap@gmail.com>
2019-05-26 15:26:00 -0300

    first commit yay

---
+++ index.js
@@ -0,0 +1,52 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+
+async function getLinks(page, type = 'lessons', selector = 'a[data-click-handler=true]') {
+  await page.waitForSelector(selector, { timeout: 0})
+
+  const links = await page.$$eval(selector, dirtyLinks => dirtyLinks.map(link => link.href))
+  const regex = new RegExp(`^https://egghead.io/${type}/`)
+  const cleanLinks = links.reduce((acc, cur) => cur.match(regex) ? `${acc}\n${cur}` : acc)
+
+  return cleanLinks
+}
+
+async function getFrameworkCourses(page, framework) {
+  await page.goto(`https://egghead.io/browse/frameworks/${framework}`)
+
+  const selector = `a.no-underline.db.dark-blue.z-1.bg-white.eh-shadow-1.css-quva1q`
+  await page.waitForSelector(selector, { timeout: 0})
+
+  const links = await page.$$eval(selector, dirtyLinks => dirtyLinks.map(link => link.href))
+  return links.filter(item => item.match(/^https:\/\/egghead.io\/courses/))
+}
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array)
+  }
+}
+
+async function run() {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  const fw = process.argv.slice(2)
+  const courses = await getFrameworkCourses(page, fw)
+
+  await asyncForEach(courses, async (item) => {
+    await page.goto(item)
+    const lessons = await getLinks(page)
+    const courseSlug = item.substr(27)
+    fs.mkdir(`./courses/${fw}/${courseSlug}`, { recursive: true }, (err) => {
+      if(err){
+        console.error(err)
+        return false
+      }
+      fs.writeFile(`./courses/${fw}/${courseSlug}/${courseSlug}.txt`, lessons, err => err ? console.error(err) : console.log(`${courseSlug} - File saved!`))
+    })
+  });
+  
+  browser.close()
+}
+
+run()
